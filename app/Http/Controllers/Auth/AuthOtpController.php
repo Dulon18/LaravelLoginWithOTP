7d147a165b6bb98c5ce7a63153ead840bfdeb78e
Dulon18 <dulon587@gmail.com>
2022-12-16 21:33:37 +0600

    update controller

--- app/Http/Controllers/Auth/AuthOtpController.php
+++ app/Http/Controllers/Auth/AuthOtpController.php
@@ -1,14 +1,20 @@
 <?php
-
-namespace App\Http\Controllers;
-
+  
+namespace App\Http\Controllers\Auth;
+  
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\OtpUser;
-
+  
 class AuthOtpController extends Controller
 {
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
     public function login()
     {
         return view('auth.otpLogin');
